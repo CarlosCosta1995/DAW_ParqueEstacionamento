875e804a7d0a3ecd9ab38e72cdf559e01d02c3e0
Carlos Costa <andrefr1995@gmail.com>
2021-12-27 16:19:29 +0000

    tentativa de fazer a base de menu

---
+++ CarlosCosta_2036114_InesFonseca_2036314/Parque de estacionamento/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parque_de_estacionamento
+{
+    public class AllOtherMenus
+    {
+        //private static int userInput = int.Parse(Console.ReadLine());
+        public static void MainMenu(int userInput) 
+        {
+            Console.WriteLine("MainMenu selected!");
+            switch (userInput)
+            {
+                case 1:
+                    Console.WriteLine("MainMenu select 1");
+                    break;
+               case 2:
+                    Console.WriteLine("MainMenu select 2");
+                    break;
+               default:
+                    CloseApp(true);
+                    break;
+            }
+        }
+
+        public static void ClientMenu()
+        {
+            Console.WriteLine("ClientMenu selected!");
+        }
+
+        public static void AdminMenu() 
+        {
+            Console.WriteLine("AdminMenu selected!");
+        }
+        
+        public static void OptionsMenu () 
+        {
+            Console.WriteLine("OptionsMenu selected!");
+        }
+        public static bool CloseApp(bool setMenuOff)
+        {
+            return false;
+        }
+    }
+
+
+}
--- CarlosCosta_2036114_InesFonseca_2036314/Parque de estacionamento/Program.cs
+++ CarlosCosta_2036114_InesFonseca_2036314/Parque de estacionamento/Program.cs
@@ -1,33 +1,75 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.Title = "A Car Slot! -  Parking agency"; //Console Tittle
+Console.ForegroundColor = ConsoleColor.Yellow; //Console Ink Color
+//Console.BackgroundColor = ConsoleColor.DarkCyan; //Console Write Color
 
 //================ Main Menu User Interface ================//
-bool activeMenu = true;
+bool activeMenu = true; //Bool to Mantain the menu always active
 while (activeMenu)
 {
-    Console.WriteLine("Welcome to StopCar agency Parking Slots!");
-    Console.WriteLine("Welcome to StopCar agency Parking Slots!"); //Colocar Data e hora
-
     int userSelection = int.Parse(Console.ReadLine()); //user input to navigate through the Main Menu
 
-    switch (userSelection)
+    switch (userSelection) //Ref: https://stackoverflow.com/questions/43484523/switch-statements-for-a-menu-c
     {
         case 0:
-            //Call client Menu
+            //call Main Menu
+            MainMenu(userSelection);
             break;
         case 1:
-            //Call Admin Menu
+            //Call client Menu
+            ClientMenu();
             break;
         case 2:
-            //Call Option Menu
+            //Call Admin Menu
+            AdminMenu();
             break;
         case 3:
-            activeMenu = false; //Closing Application
+            //Call Option Menu
+            OptionsMenu();
+            break;
+        case 4:
+            activeMenu = CloseApp(activeMenu); //Closing Application
             break;
         default:
             //Unnexpected action from the user. What should it happen in this case?
             break;
-
     }
 
+
+    static void MainMenu(int userInput)
+    {
+        Console.WriteLine("MainMenu selected!");
+        switch (userInput)
+        {
+            case 1:
+                Console.WriteLine("MainMenu select 1");
+                break;
+            case 2:
+                Console.WriteLine("MainMenu select 2");
+                break;
+            default:
+                CloseApp(true);
+                break;
+        }
+    }
+
+    static void ClientMenu()
+    {
+        Console.WriteLine("ClientMenu selected!");
+    }
+
+    static void AdminMenu()
+    {
+        Console.WriteLine("AdminMenu selected!");
+    }
+
+    static void OptionsMenu()
+    {
+        Console.WriteLine("OptionsMenu selected!");
+    }
+    static bool CloseApp(bool setMenuOff)
+    {
+        return false;
+    }
 }
