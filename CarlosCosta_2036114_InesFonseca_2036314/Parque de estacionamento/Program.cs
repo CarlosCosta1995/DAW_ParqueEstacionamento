4b35ef84d464f11ee67dd19ffff172e308aa3c92
Carlos Costa <andrefr1995@gmail.com>
2021-12-27 13:55:17 +0000

    Construindo a Base do Menu
    Construindo apenas a base necessária para as outras funcionalidades.

---
+++ CarlosCosta_2036114_InesFonseca_2036314/Parque de estacionamento/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//================ Main Menu User Interface ================//
+bool activeMenu = true;
+while (activeMenu)
+{
+    Console.WriteLine("Welcome to StopCar agency Parking Slots!");
+    Console.WriteLine("Welcome to StopCar agency Parking Slots!"); //Colocar Data e hora
+
+    int userSelection = int.Parse(Console.ReadLine()); //user input to navigate through the Main Menu
+
+    switch (userSelection)
+    {
+        case 0:
+            //Call client Menu
+            break;
+        case 1:
+            //Call Admin Menu
+            break;
+        case 2:
+            //Call Option Menu
+            break;
+        case 3:
+            activeMenu = false; //Closing Application
+            break;
+        default:
+            //Unnexpected action from the user. What should it happen in this case?
+            break;
+
+    }
+
+}
