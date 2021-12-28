a26b1ffaf4b5101b1de96721ce51982835770c5f
Carlos Costa <andrefr1995@gmail.com>
2021-12-28 17:44:41 +0000

    try 1

---
+++ CarlosCosta_2036114_InesFonseca_2036314/Parque de estacionamento/Functionalities.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Menus;
+
+namespace Functionalities
+{
+    //Method that receives the car plates inserted by the user
+    //Stores into the Arrays
+    //Outputs a list of plates and a quantity of plate. 
+    public class CarPlate 
+    {
+        //Receive Car Plates and store in the next variables.
+        public string _carPlateZone1;
+        public string _carPlateZone2;
+        public string _carPlateZone3;
+
+        //Creating an Array to store the plates.
+        public string[] _plateArrayZone1 = { };
+        public string[] _plateArrayZone2 = { };
+        public string[] _plateArrayZone3 = { };
+
+        //Stores the arrays.Length into a variable
+        public int carsInZone1 = 0;
+        private int carsInZone2 = 0;
+        private int carsInZone3 = 0;
+
+        //Atribute values to the current class from outside sources.
+        public CarPlate(string carPlateZone1, string carPlateZone2, string carPlateZone3) 
+        {
+            this._carPlateZone1 = carPlateZone1;
+            this._carPlateZone2 = carPlateZone2;
+            this._carPlateZone3 = carPlateZone3;
+        }
+        //Store and Counts Array elements for Zone1
+        public Array GetCarPlateZone1(string _carPlateZone1) 
+        {
+            _plateArrayZone1 = new string[] { _carPlateZone1 };
+            return _plateArrayZone1;
+        }
+        public int occupiedSlotsZone1(string carPlateZone1) 
+        {
+            carsInZone1 = _carPlateZone1.Length;
+            return carsInZone1;
+            //return _carPlateZone1.Length;
+        }
+
+        //Store and count Array elements for Zone2
+        public Array GetCarPlateZone2(string _carPlateZone2)
+        {
+            _plateArrayZone2 = new string[] { _carPlateZone2 };
+            return _plateArrayZone2;
+        }
+        public int occupiedSlotsZone2()
+        {
+            carsInZone2 = _carPlateZone1.Length;
+            return carsInZone2;
+        }
+
+        //Store and count Array elements for Zone3
+        public Array GetCarPlateZone3(string _carPlateZone3)
+        {
+            _plateArrayZone3 = new string[] { _carPlateZone3 };
+            return _plateArrayZone3;
+        }
+        public int occupiedSlotsZone3()
+        {
+            carsInZone3 = _carPlateZone1.Length;
+            return carsInZone3;
+        }
+    }
+
+    public class ZoneAvailability
+    {
+        //Define and atribute variables for each zone
+        public int carSlotsZone1 = 50;
+        public int carSlotsZone2 = 100;
+        public int carSlotsZone3;
+
+        //Quantity of cars in the zone
+        public int _numCarInZone1;
+        public int _numCarInZone2;
+        public int _numCarInZone3;
+
+        public ZoneAvailability(int carsInZone1, int carsInZone2, int carsInZone3) 
+        {
+            this._numCarInZone1 = carsInZone1;
+            this._numCarInZone2 = carsInZone2;
+            this._numCarInZone3 = carsInZone3;  
+        }
+        public static void CountingCarsZone1() 
+        {
+            
+        }
+        public static void CountingCarsZone2()
+        {
+
+        }
+        public static void CountingCarsZone3()
+        {
+
+        }
+    }
+}
---
+++ CarlosCosta_2036114_InesFonseca_2036314/Parque de estacionamento/Menus.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Functionalities;
+
+namespace Menus
+{
+    public class AllMenus
+    {
+        public static void MainMenu() 
+        {
+            Console.Clear();
+
+                Console.WriteLine("\nWelcome to CarSlot Parking Agency!");
+                Console.WriteLine(DateTime.Now); //Display date and time
+                Console.WriteLine("Navigate through the Menus by presseing the representative number!");
+                Console.WriteLine("Client Menu [0]");
+                Console.WriteLine("Admin Menu [1]");
+                Console.WriteLine("Options [2]");
+                Console.WriteLine("Leave Application [3]");
+                
+                int userSelection = int.Parse(Console.ReadLine()); //user input to navigate through the Main Menu
+
+                switch (userSelection) //Ref: https://stackoverflow.com/questions/43484523/switch-statements-for-a-menu-c
+                {
+                    case 0:
+                        //Call client Menu
+                        ClientMenu();
+                        break;
+                    case 1:
+                        //Call Admin Menu
+                        //AdminMenu();
+                        AdminLogInMenu();
+                        break;
+                    case 2:
+                        //Call Option Menu
+                        OptionsMenu();
+                        break;
+                    case 3:
+                        CloseApp(); //Closing Application
+                        break;
+                    default:
+                        //Unnexpected action from the user. What should it happen in this case?
+                        Console.WriteLine("choose only bettween the numbers on the console, please!");
+                        break;
+                }
+            }
+        public static void ClientMenu() //https://www.youtube.com/watch?v=9JST13MhrFU
+        {
+            Console.Clear();
+
+            Console.WriteLine("\nClientMenu selected!");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("     >Schedule<    ");
+            Console.WriteLine("----working days----");
+            Console.WriteLine(" 9:00 am - 8:00 pm ");
+            Console.WriteLine("-----saturdays------");
+            Console.WriteLine(" 9:00 am - 2:00 pm ");
+            Console.WriteLine(">Choose a zone number<");
+            Console.WriteLine("zone 1");
+            Console.WriteLine("zone 2");
+            Console.WriteLine("zone 3");
+            Console.WriteLine("Back to Main Menu [0]");
+            Console.WriteLine("Leave Application [5]");
+
+            int zone = int.Parse(Console.ReadLine());
+
+            switch (zone)
+            {
+                case 0:
+                    MainMenu();
+                    break;
+                case 1:
+                    Console.WriteLine("\nZone 1 selected");
+                    Console.WriteLine("-Price/máx.Hour-");
+                    Console.WriteLine("1.15€/h - max.45 min");
+                    ClientMenu();
+                    break;
+                case 2:
+                    Console.WriteLine("\nZone 2 selected");
+                    Console.WriteLine("-Price/máx.Hour-");
+                    Console.WriteLine("1€/h - max.2 hours");
+                    ClientMenu();
+                    break;
+                case 3:
+                    Console.WriteLine("\nZone 3 selected");
+                    Console.WriteLine("-Price/máx.Hour-");
+                    Console.WriteLine("0.62€/h - no max.hours");
+                    ClientMenu();
+                    break;
+                case 4:
+                    //CloseApp(); //Closing Application
+                    break;
+                case 5:
+                    CloseApp(); //Closing Application
+                    break;
+                default:
+                    //Unnexpected action from the user. What should it happen in this case?
+                    Console.WriteLine("choose only bettween the numbers on the console, please!");
+                    break;
+            }
+
+            /*
+            if (zone == 0)
+            {
+                MainMenu();
+            }
+            if (zone == 1)
+            {
+                Console.WriteLine("Zone 1 selected");
+                Console.WriteLine("-Price/máx.Hour-");
+                Console.WriteLine("1.15€/h - max.45 min");
+       
+    
+            }
+
+            if (zone == 2)
+            {
+                Console.WriteLine("Zone 2 selected");
+                Console.WriteLine("-Price/máx.Hour-");
+                Console.WriteLine("1€/h - max.2 hours");
+
+            }
+
+            if (zone == 3)
+            {
+                Console.WriteLine("Zone 3 selected");
+                Console.WriteLine("-Price/máx.Hour-");
+                Console.WriteLine("0.62€/h - no max.hours");
+
+
+            }
+            if (zone == 5)
+            {
+                CloseApp();
+            }*/
+        }
+
+        public static void AdminLogInMenu() 
+        {
+            Console.Clear();
+
+            int pinCode = 0987;
+
+            Console.WriteLine("Hello! Please, insert your Admin Pin code.\n");
+            int pin = int.Parse(Console.ReadLine());
+
+                if (pin == pinCode)
+                {
+                    Console.WriteLine("Your Pin is correct!\n");
+                    AdminMenu();
+                }
+                /*else if (pin != pinCode && tries < 3)
+                {
+                    Console.WriteLine("Your Pin is Incorrect! Please try again.\n");
+                    AdminLogInMenu();
+                    tries++;
+                }*/
+                else if (pin != pinCode)
+                {
+                    Console.WriteLine("Blocked, you will be sent back to the main Menu. Goodbye!");
+                    MainMenu();
+                }
+        }
+
+        public static void AdminMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("\nAdminMenu selected!");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("     >Schedule<    ");
+            Console.WriteLine("----working days----");
+            Console.WriteLine(" 9:00 am - 8:00 pm ");
+            Console.WriteLine("-----saturdays------");
+            Console.WriteLine(" 9:00 am - 2:00 pm ");
+            Console.WriteLine(">Choose a zone number<");
+            Console.WriteLine("zone 1");
+            Console.WriteLine("zone 2");
+            Console.WriteLine("zone 3");
+            Console.WriteLine("Back to Main Menu [0]");
+            Console.WriteLine("Back to Admin Main Menu [4]");
+            Console.WriteLine("Leave Application [5]");
+
+
+            int zone = int.Parse(Console.ReadLine());
+
+            switch (zone)
+            {
+                case 0:
+                    MainMenu();
+                    break;
+                case 1:
+                    Console.WriteLine("Zone 1 selected");
+                    Console.WriteLine("-Price/máx.Hour-");
+                    Console.WriteLine("1.15€/h - max.45 min");
+                    AdminMenu();
+                    break;
+                case 2:
+                    Console.WriteLine("Zone 2 selected");
+                    Console.WriteLine("-Price/máx.Hour-");
+                    Console.WriteLine("1€/h - max.2 hours");
+                    AdminMenu();
+                    break;
+                case 3:
+                    Console.WriteLine("Zone 3 selected");
+                    Console.WriteLine("-Price/máx.Hour-");
+                    Console.WriteLine("0.62€/h - no max.hours");
+                    AdminMenu();
+                    break;
+                case 4:
+                    CloseApp(); //Closing Application
+                    break;
+                case 5:
+                    AdminMenu();
+                    break;
+                default:
+                    //Unnexpected action from the user. What should it happen in this case?
+                    Console.WriteLine("choose only bettween the numbers on the console, please!");
+                    break;
+            }
+            /*
+            if (zone == 1)
+            {
+                Console.WriteLine("Zone 1 selected");
+                Console.WriteLine("-Price/máx.Hour-");
+                Console.WriteLine("1.15€/h - max.45 min");
+
+
+            }
+
+            if (zone == 2)
+            {
+                Console.WriteLine("Zone 2 selected");
+                Console.WriteLine("-Price/máx.Hour-");
+                Console.WriteLine("1€/h - max.2 hours");
+
+            }
+
+            if (zone == 3)
+            {
+                Console.WriteLine("Zone 3 selected");
+                Console.WriteLine("-Price/máx.Hour-");
+                Console.WriteLine("0.62€/h - no max.hours");
+
+
+            }*/
+
+        }
+
+        public static void OptionsMenu ()
+        {
+            Console.Clear();
+
+            Console.WriteLine("OptionsMenu selected!");
+        }
+        public static void CloseApp()
+        {
+
+            Environment.Exit(0); //Command to Close App
+        }
+    }
+}
