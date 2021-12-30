25e56933b2b027eb7c679f7cd6b23fcad43e5618
inesfonseca2021 <94610096+inesfonseca2021@users.noreply.github.com>
2021-12-30 15:36:48 +0000

    main update
    error zone 3
    back to client menu from zones

--- CarlosCosta_2036114_InesFonseca_2036314/Parque de estacionamento/Program.cs
+++ CarlosCosta_2036114_InesFonseca_2036314/Parque de estacionamento/Program.cs
@@ -18,170 +18,185 @@ Console.WriteLine("Loading...");
 bool activeMenu = true; //Bool to Mantain the menu always active
 while (activeMenu)
 
-    {
-        Console.WriteLine("start [0]"); //stand by and also a home button in all the menu
-        int userSelectionMenu = int.Parse(Console.ReadLine()); //user input to navigate through the Menu
-    
+{
+    Console.WriteLine("start [0]"); //stand by and also a home button in all the menu
+    int userSelectionMenu = int.Parse(Console.ReadLine()); //user input to navigate through the Menu
+
     switch (userSelectionMenu) //to be able to choose a number, Ref: https://stackoverflow.com/questions/43484523/switch-statements-for-a-menu-c
-        {
-            case 0: //Main Menu
+    {
+        case 0: //Main Menu
 
-                Console.WriteLine("Welcome to CarSlot Parking Agency!");
-                Console.WriteLine(DateTime.Now); //display date and time
-                Console.WriteLine("Navigate through the Menus by presseing the representative number!");
-                Console.WriteLine("Client Menu [1]");
-                Console.WriteLine("Admin Menu [2]");               
-                break; //to prevent the other options from appering ate the same time
+            Console.WriteLine("Welcome to CarSlot Parking Agency!");
+            Console.WriteLine(DateTime.Now); //display date and time
+            Console.WriteLine("Navigate through the Menus by presseing the representative number!");
+            Console.WriteLine("Client Menu [1]");
+            Console.WriteLine("Admin Menu [2]");
+            break; //to prevent the other options from appering ate the same time
 
-            case 1: //Client Menu
-                                
-                Console.WriteLine("ClientMenu selected!");
-                Console.WriteLine(DateTime.Now);
-                Console.WriteLine("     >Schedule<    ");
-                Console.WriteLine("----working days----");
-                Console.WriteLine(" 9:00 am - 8:00 pm ");
-                Console.WriteLine("-----saturdays------");
-                Console.WriteLine(" 9:00 am - 2:00 pm ");
-                Console.WriteLine(">Choose a zone number<");
-                Console.WriteLine("_zone [1]");
-                Console.WriteLine("_zone [2]");
-                Console.WriteLine("_zone [3]");
-                Console.WriteLine("back to main menu [5]");
+        case 1: //Client Menu
+
+            Console.WriteLine("ClientMenu selected!");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("     >Schedule<    ");
+            Console.WriteLine("----working days----");
+            Console.WriteLine(" 9:00 am - 8:00 pm ");
+            Console.WriteLine("-----saturdays------");
+            Console.WriteLine(" 9:00 am - 2:00 pm ");
+            Console.WriteLine(">Choose a zone number<");
+            Console.WriteLine("_zone [1]");
+            Console.WriteLine("_zone [2]");
+            Console.WriteLine("_zone [3]");
+            Console.WriteLine("back to main menu [4]");
+            
+
+
+            int userSelectionClientMenu = int.Parse(Console.ReadLine()); //choice of the number of the zone
+
+            if (userSelectionClientMenu == 4)
+            {
+                goto case 0; //ref. https://www.xsprogram.com/content/how-to-jump-to-another-case-statement-in-a-switch-case-condition-with-the-value.html
+            }
+
+            else if (userSelectionClientMenu >= 5)
+            {
+                goto default; //ref. https://www.xsprogram.com/content/how-to-jump-to-another-case-statement-in-a-switch-case-condition-with-the-value.html
+            }
+
+            /*int backClientMenu = int.Parse(Console.ReadLine()); //choice to go back to the client menu*/
+            /*if (userSelectionClientMenu == 6)
+            {
+                return; //ref. https://www.xsprogram.com/content/how-to-jump-to-another-case-statement-in-a-switch-case-condition-with-the-value.html
+
+            }*/
+
+
+            if (userSelectionClientMenu == 1)
+            {
+
+                Console.WriteLine("Zone 1 selected");
+                Console.WriteLine("---Price/max.Hour---");
+                Console.WriteLine("1.15€/h - max.45 min");
+                Zones();
+
                 
-                        
-                        int userSelectionClientMenu = int.Parse(Console.ReadLine()); //choice of the number of the zone
-
-                        if (userSelectionClientMenu == 5)
-                        {
-                            goto case 0; //ref. https://www.xsprogram.com/content/how-to-jump-to-another-case-statement-in-a-switch-case-condition-with-the-value.html
-                        }
-
-                        /*int backClientMenu = int.Parse(Console.ReadLine()); //choice to go back to the client menu*/
-                        /*if (userSelectionClientMenu == 6)
-                        {
-                            return; //ref. https://www.xsprogram.com/content/how-to-jump-to-another-case-statement-in-a-switch-case-condition-with-the-value.html
-                            
-                        }*/
 
 
-                        if (userSelectionClientMenu == 1)
-                        {
-                            Console.WriteLine("Zone 1 selected");
-                            Console.WriteLine("---Price/max.Hour---");
-                            Console.WriteLine("1.15€/h - max.45 min");
-                            Zones();
-                            
+                string ticket = Console.ReadLine();
+                if (ticket == "y")
+                {
+                    Console.WriteLine("ticket");
+                    Console.WriteLine("----zone 1----");
+                    Console.WriteLine(DateTime.Now);
+                    Console.WriteLine("You can be parked for x time.");
+                    Console.WriteLine("I want to print another ticket [choiceZone]");
+                    Console.WriteLine("done [X]");
 
-                            string ticket = Console.ReadLine();
-                            if (ticket == "y")
-                            {
-                                Console.WriteLine("ticket");
-                                Console.WriteLine("----zone 1----");
-                                Console.WriteLine(DateTime.Now);
-                                Console.WriteLine("You can be parked for x time.");
-                                Console.WriteLine("I want to print another ticket [choiceZone]");
-                                Console.WriteLine("done [X]");
+                    string done = Console.ReadLine();
+                    if (done == "x")
+                    {
+                        Console.WriteLine("thank you, BYE");
+                        //CloseApp();
+                    }
+                }
 
-                                    string done = Console.ReadLine();
-                                    if (done == "x")
-                                    {
-                                        Console.WriteLine("thank you, BYE");
-                                        //CloseApp();
-                                    }
-                            }
-
-                            if (ticket == "n")
-                            {
-                                Console.WriteLine("Colect your money back and start again");
-                            }
-
-                        }
-
-
-                        if (userSelectionClientMenu == 2)
-                        {
-                            Console.WriteLine("Zone 2 selected");
-                            Console.WriteLine("---Price/max.Hour---");
-                            Console.WriteLine("1€/h - max.2 hours");
-                            Zones();
-
-                            string ticket = Console.ReadLine();
-                            if (ticket == "y")
-                            {
-                                Console.WriteLine("ticket");
-                                Console.WriteLine("----zone 1----");
-                                Console.WriteLine(DateTime.Now);
-                                Console.WriteLine("You can be parked for x time.");
-                                Console.WriteLine("I want to print another ticket");
-                                Console.WriteLine("done [X]");
-
-                                string done = Console.ReadLine();
-                                if (done == "x")
-                                {
-                                    Console.WriteLine("thank you, BYE");
-                                    //CloseApp();
-                                }
-
-                            if (ticket == "n")
-                            {
-                                Console.WriteLine("Colect your money back and start again");
-                            }
-
-                        }
-
-
-                        /*if (userSelectionClientMenu == 3)
-                        {
-                            Console.WriteLine("Zone 3 selected");
-                            Console.WriteLine("---Price/max.Hour---");
-                            Console.WriteLine("0.62€/h - no max.hours");
-                            Zones();
-
-                            string ticket = Console.ReadLine();
-                            if (ticket == "y")
-                            {
-                                Console.WriteLine("ticket");
-                                Console.WriteLine("----zone 1----");
-                                Console.WriteLine(DateTime.Now);
-                                Console.WriteLine("You can be parked for x time.");
-                                Console.WriteLine("I want to print another ticket [choiceZone]");
-                                Console.WriteLine("done [X]");
-
-                                string done = Console.ReadLine();
-                                if (done == "x")
-                                {
-                                    Console.WriteLine("thank you, BYE");
-                                    //goto case 5;
-                                    //CloseApp();
-                                }   
-
-                        if (ticket == "n")
-                        {
-                            Console.WriteLine("Colect your money back and start again");
-                        }
-                        }
-                    
-                        }*/
+                if (ticket == "n")
+                {
+                    Console.WriteLine("Colect your money back and start again");
+                }
+                else 
+                {
+                    goto default; //ref. https://www.xsprogram.com/content/how-to-jump-to-another-case-statement-in-a-switch-case-condition-with-the-value.html
+                }
                 
+
+            }
+
+
+            if (userSelectionClientMenu == 2)
+            {
+                Console.WriteLine("Zone 2 selected");
+                Console.WriteLine("---Price/max.Hour---");
+                Console.WriteLine("1€/h - max.2 hours");
+                Zones();
+
+                string ticket = Console.ReadLine();
+                if (ticket == "y")
+                {
+                    Console.WriteLine("ticket");
+                    Console.WriteLine("----zone 1----");
+                    Console.WriteLine(DateTime.Now);
+                    Console.WriteLine("You can be parked for x time.");
+                    Console.WriteLine("I want to print another ticket");
+                    Console.WriteLine("done [X]");
+
+                    string done = Console.ReadLine();
+                    if (done == "x")
+                    {
+                        Console.WriteLine("thank you, BYE");
+                        //CloseApp();
+                    }
+
+                    if (ticket == "n")
+                    {
+                        Console.WriteLine("Colect your money back and start again");
+                    }
+
+                }
+                else
+                {
+                    goto default; //ref. https://www.xsprogram.com/content/how-to-jump-to-another-case-statement-in-a-switch-case-condition-with-the-value.html
+                }
+
+
+                /*if (userSelectionClientMenu == 3)
+                {
+                    Console.WriteLine("Zone 3 selected");
+                    Console.WriteLine("---Price/max.Hour---");
+                    Console.WriteLine("0.62€/h - no max.hours");
+                    Zones();
+                    string ticket = Console.ReadLine();
+                    if (ticket == "y")
+                    {
+                        Console.WriteLine("ticket");
+                        Console.WriteLine("----zone 1----");
+                        Console.WriteLine(DateTime.Now);
+                        Console.WriteLine("You can be parked for x time.");
+                        Console.WriteLine("I want to print another ticket [choiceZone]");
+                        Console.WriteLine("done [X]");
+                        string done = Console.ReadLine();
+                        if (done == "x")
+                        {
+                            Console.WriteLine("thank you, BYE");
+                            //goto case 5;
+                            //CloseApp();
+                        }   
+                if (ticket == "n")
+                {
+                    Console.WriteLine("Colect your money back and start again");
+                }
+                }
+
+                }*/
+
             }
             break;
 
-            case 2: //Call Admin Menu
+        case 2: //Call Admin Menu
                 //AdminMenu();
-                break;
+            break;
 
-            case 5://back to main menu from the client menu, cant be here because when we start and choose 6 it goes do client menu
+        case 5://back to main menu from the client menu, cant be here because when we start and choose 6 it goes do client menu
             goto case 0;
-                break;
+            break;
 
-            case 6: //back to client menu from any zone, cant be here because when we start and choose 6 it goes do client menu
+        case 6: //back to client menu from any zone, cant be here because when we start and choose 6 it goes do client menu
             goto case 1;
             break;
 
 
-            default: //what happends if the client chooses a letter/number that is not on the script, doesn´t work on client menu
-                Console.WriteLine("Please, choose only the numbers available in the script");
-                break;
+        default: //what happends if the client chooses a letter/number that is not on the script, doesn´t work on client menu
+            Console.WriteLine("Please, choose only the numbers available in the script");
+            break;
     }
 }
 
@@ -200,15 +215,15 @@ static void Zones()
     Console.WriteLine("Insert your car plate:");
     Console.WriteLine("You can be parked for x hours.");
     Console.WriteLine("Do you agree? type [y] or [n].");
-    Console.WriteLine("Go back to Client menu [6]");
+    Console.WriteLine("Go back to Client menu [4]");
     Console.WriteLine("start [0]");
-    
+
 }
 
-static bool CloseApp (bool setMenuOff)
+static bool CloseApp(bool setMenuOff)
 {
     return false;
 }
 
-
-
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
